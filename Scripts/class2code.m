71c370c344e61d1b424971e5f8fc3bbf4f429a99
Hellder <bueno.h@aluno.ufabc.edu.br>
2023-09-11 02:22:20 -0300

    Frames of Reference

---
+++ Scripts/class2code.m
@@ -0,0 +1,22 @@
+﻿% variáveis angulares
+
+syms psi theta phi
+
+
+% matrizes
+
+Lpsi = [cos(psi) sin(psi) 0; -sin(psi) cos(psi) 0; 0 0 1];
+
+Ltheta = [cos(theta) 0 -sin(theta); 0 1 0; sin(theta) 0 cos(theta)];
+
+Lphi = [1 0 0; 0 cos(phi) sin(phi); 0 -sin(phi) cos(phi)];
+
+
+% matriz de rotação terra corpo
+
+LEB = Lphi*Ltheta*Lpsi
+
+
+% matriz de rotação corpo terra
+
+LBE = LEB'
---
+++ Scripts/class2ex.m
@@ -0,0 +1,31 @@
+﻿% variáveis angulares
+
+syms psi theta phi g We Wb
+
+
+
+% matrizes
+
+Lpsi = [cos(psi) sin(psi) 0; -sin(psi) cos(psi) 0; 0 0 1];
+
+Ltheta = [cos(theta) 0 -sin(theta); 0 1 0; sin(theta) 0 cos(theta)];
+
+Lphi = [1 0 0; 0 cos(phi) sin(phi); 0 -sin(phi) cos(phi)];
+
+
+
+% vetor peso no eixo da terra
+We = [0; 0; g];
+
+
+
+% matriz de rotação terra corpo
+
+LEB = Lphi*Ltheta*Lpsi;
+
+
+
+% convertendo We para o eixo do corpo
+
+
+Wb = LEB * We
